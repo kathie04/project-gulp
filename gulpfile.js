db331bd6d93ee0648d00e255f7183e3fd4d34c39
Katerina <kathie2709@gmail.com>
2019-10-15 15:08:01 +0300

    add new tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 'use strict';
 
-var gulp        = require('gulp'); // gulp
-var sass        = require('gulp-sass'); // compile scss to css
-var imageop     = require('gulp-image-optimization'); // image optimization
-var include     = require('gulp-include'); //
-var browserSync = require('browser-sync'); // live reload browser
-var imagemin    = require('gulp-imagemin'); // Подключаем библиотеку для работы с изображениями
-var pngquant    = require('imagemin-pngquant'); // Подключаем библиотеку для работы с png
-var debug       = require('gulp-debug');
-var del         = require('del');
-var pug         = require('gulp-pug');
+var gulp         = require('gulp'); // gulp
+var sass         = require('gulp-sass'); // compile scss to css
+var imageop      = require('gulp-image-optimization'); // image optimization
+var include      = require('gulp-include'); //
+var browserSync  = require('browser-sync'); // live reload browser
+var imagemin     = require('gulp-imagemin'); // Подключаем библиотеку для работы с изображениями
+var pngquant     = require('imagemin-pngquant'); // Подключаем библиотеку для работы с png
+var debug        = require('gulp-debug');
+var del          = require('del');
+var sourcemaps   = require('gulp-sourcemaps');
+var cleanCSS     = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('img', function() {
   return gulp.src('./src/images/**/*') // Берем все изображения из src
@@ -35,28 +37,23 @@ gulp.task('images', function(cb) {
 
 
 gulp.task('browser-sync', function () {
-  browserSync({ 
-      server: {
-          baseDir: './dist'
-      },
+  browserSync({
+      server: './',
+      directory: true,
+      index: "index.html",
       notify: false
   });
 });
 
-gulp.task('pug', function() {
-  return gulp.src("./src/**/*.pug")
-    .pipe(pug({
-      pretty: true
-    }))  
-    .pipe(gulp.dest("./src"))
-    .pipe(browserSync.stream());
-});
-
-gulp.task('html', ['pug'], function() {
-  return gulp.src('./src/index.html')
-  .pipe(gulp.dest('./dist'))
-  .pipe(browserSync.reload({stream: true})); // Обновляем
-});
+gulp.task('html', () =>
+    gulp.src('./src/*.html')
+        .pipe(include({
+            prefix: '@@',
+            basepath: './src/modules/'
+        }))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(browserSync.reload({stream: true}))
+)
 
 gulp.task('fonts', function() {
   return gulp.src('./src/fonts/**/*.*')
@@ -73,11 +70,18 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./src/styles/**/*.{scss, sass}')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/styles')) // Выгружаем результата в папку app/css
-    .pipe(browserSync.reload({stream: true})); // Обновляем CSS на странице при изменении
-});
+        return gulp.src('./src/styles/**/*.{scss, sass}')
+            .pipe(sourcemaps.init())
+            .pipe(sass().on('error', sass.logError))
+            .pipe(cleanCSS({compatibility: 'ie8'}))
+            .pipe(autoprefixer({
+                cascade: false
+            }))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('./dist/css'))
+            .pipe(browserSync.reload({stream: true}));
+    });
+
 
 gulp.task('clean', function(){
   return del.sync('dist');
@@ -87,7 +91,9 @@ gulp.task('build', ['clean', 'sass', 'fonts', 'scripts', 'html', 'img'], functio
 });
 
 gulp.task('watch', ['browser-sync', 'build'], function () {
-  gulp.watch('./src/**/*.pug', ['pug', 'html']);
+  gulp.watch('./src/**/*.html', ['html']);
   gulp.watch('./src/styles/**/*.{scss, sass}', ['sass']);
   gulp.watch('./src/js/**/*.js', ['js']);
 })
+
+gulp.task('default', ['watch']);
